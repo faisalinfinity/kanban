5f80c641757d21b035839d910896f70c19a25be9
faisalinfinity <mujtabafaisal944.fk@gmail.com>
2025-01-24 19:47:42 +0530

    removed unused vars

--- src/components/TaskDetails.tsx
+++ src/components/TaskDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Box, Button, MenuItem, Select, TextField, Typography } from "@mui/material";
 
